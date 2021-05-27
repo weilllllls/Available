72d3cf4bbb671ebd59962c469a8484dcbf78cc43
DOSSVROD <38550307+1820670352@users.noreply.github.com>
2021-05-27 21:03:22 +0800

    Delete code/JavaWeb directory

--- code/JavaWeb/src/com/zqb/utils/PublicInfo.java
+++
@@ -1,7 +0,0 @@
-package com.zqb.utils;
-
-public class PublicInfo {
-//    public static final String DBINFO = "jdbc:mysql://localhost:3306/test?user=good&password=good&useUnicode=true&characterEncoding=UTF-8";
-    public static final String DBINFO = "jdbc:mysql://localhost:3366/ydbc2020?user=Administrator&password=XWClassroom20202023&useUnicode=true&characterEncoding=UTF-8";
-
-}
